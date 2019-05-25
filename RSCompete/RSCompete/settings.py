4e8a7aecfdae7ed5c26e69c84dd4945d48ea4bab
Shiyu Xuan <464430440@qq.com>
2019-05-25 15:17:14 +0800

    测试用使邮箱无法发送

--- RSCompete/RSCompete/settings.py
+++ RSCompete/RSCompete/settings.py
@@ -35,7 +35,7 @@ EMAIL_USE_SSL = True    #是否使用SSL加密，qq企业邮箱要求使用
 EMAIL_HOST = 'smtp.163.com'   #发送邮件的邮箱 的 SMTP服务器，这里用了163邮箱
 EMAIL_PORT = 465     #发件箱的SMTP服务器端口
 EMAIL_HOST_USER = 'a464430440@163.com'    #发送邮件的邮箱地址
-EMAIL_HOST_PASSWORD = 'xuanshiyu950204'         #发送邮件的邮箱密码(这里使用的是授权码
+EMAIL_HOST_PASSWORD = 'xuanshiyu95020'         #发送邮件的邮箱密码(这里使用的是授权码
 
 # Application definition
 
