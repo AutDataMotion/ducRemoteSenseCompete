7dbaeedd4f4e0d9e3b3d4222ce7792dc35934ee4
zhongweng <zhongweng.hao@qq.com>
2019-06-14 16:59:12 +0800

    数据发布前的准备

--- RSCompete/RSCompeteAPI/default_settings.py
+++ RSCompete/RSCompeteAPI/default_settings.py
@@ -14,13 +14,15 @@ class System_Config():
     detection_gt = "/data/detection/test_challenge_c1_gt"
     detection_test_image_path = "/data/detection/visualize_images"
     tracking_gt = "/data/Stage_1_groundtruth"    
-    upload_count_perday = 50
+    upload_count_perday = 0
     current_stage = "注册中" #目前的状态
+    # current_stage = "数据发布"
     deadline = "2019/08/10"
 
     admin_username = "csu"
     admin_passwd = "csucsucsu"
 
     data_path = {1:{"url":"数据将于2019年6月15日发布，请耐心等待！", "code":""}, 2:{"url":"数据将于2019年6月15日发布，请耐心等待！", "code":""}, 3:{"url":"数据将于2019年6月15日发布，请耐心等待！", "code":""}, 4:{"url":"数据将于2019年6月15日发布，请耐心等待！", "code":""}, 5:{"url":"数据将于2019年6月15日发布，请耐心等待！", "code":""}}
+    #data_path = {1:{"url":"https://pan.baidu.com/s/1pw8SmeZ23VRSLXquefiRTA", "code":"hms2"}, 2:{"url":"https://pan.baidu.com/s/1SYgYm85sJo7TrvmDm0d0zQ", "code":"3neo"}, 3:{"url":"https://pan.baidu.com/s/1gx4Si7S17Uqe3bt_cVkULw", "code":"v785"}, 4:{"url":"https://pan.baidu.com/s/1BsGTuRkWAR-y2LO5GGTsoA", "code":"p4cf"}, 5:{"url":"https://pan.baidu.com/s/1GXJHQRn9MtqtuZ-D73c26Q", "code":"prle"}}
 
     #竞赛类型 1-目标检测 2-场景分类 3-语义分割 4-变化检测 5-目标追踪
